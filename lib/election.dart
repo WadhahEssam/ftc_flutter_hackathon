3beb25044189b8c4f6b4361414dbd94b3649ba05
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-02-01 23:52:17 +0300

    candidates view working ( not bad )

--- lib/election.dart
+++ lib/election.dart
@@ -6,7 +6,7 @@ class ElectionPage extends StatefulWidget {
   ElectionPageState createState() => new ElectionPageState();
 }
 
-class ElectionPageState extends State<ElectionPage> {
+class ElectionPageState extends State<ElectionPage> with WidgetsBindingObserver{
   int _selectedIndex = 0;
 
   @override
@@ -27,7 +27,44 @@ class ElectionPageState extends State<ElectionPage> {
   }
 
   Widget _candidatesView() {
-    return Center(child: Text('انتخابات النادي'));
+    return Center(
+      child: Container(
+        child: ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemCount: candidateNames.length,
+          itemBuilder: (BuildContext ctxt, int i) {
+            return _candidateCard(candidateNames[i], candidatePlans[i], candidateImages[i]);
+          }
+        ),
+      )
+    );
+  }
+
+  Widget _candidateCard(String name, String plan, String image) {
+    return 
+      Card(
+        child: Column(children: [
+          Container(
+            alignment: Alignment.centerRight,
+            child: Row(
+              children: [
+                Container(child: Text(name), padding: EdgeInsets.only(right: 20),),
+                CircleAvatar(child: Image.asset('assets/images/profile_image.jpg'), backgroundColor: Colors.brown.shade800,),
+                // RaisedButton(child: Text('اسئلة المرشح'), onPressed: _seeCandidatePage)
+              ], 
+              mainAxisAlignment: MainAxisAlignment.end), 
+            padding: EdgeInsets.all(15),
+          ),
+          Container(child: Column(children: <Widget>[
+            Text('الخطة', style: TextStyle(fontSize: 13)),
+            Container(child: Text(plan, style: TextStyle(fontSize: 10)), padding: EdgeInsets.all(20)),
+            Container(
+              child: RaisedButton(child: Text('التصويت', style: TextStyle(fontSize: 10, color: Colors.white)), onPressed: _seeCandidatePage, color: Theme.of(context).accentColor,),
+              margin: EdgeInsets.only(bottom: 10), 
+            )
+          ])) 
+        ])
+      );
   }
 
   Widget _candidatureView() {
@@ -62,5 +99,31 @@ class ElectionPageState extends State<ElectionPage> {
 
   void _submitCandidate() {
 
+  }
+
+  void _seeCandidatePage() {
+    
+  }
+
+
+  final candidateNames = ['اسامة الفيفي', 'ابراهيم الفرحان', 'عبدالرحمن السنيدي'];
+  final candidatePlans = [
+    'خطة اسامة الفيفي خطة اسامة الفيفي خطة اسامة الفيفي خطة اسامة الفيفي خطة اسامة الفيفي خطة اسامة الفيفي خطة اسامة الفيفي', 
+    'Make FTC Great Again Make FTC Great Again Make FTC Great Again Make FTC Great Again Make FTC Great Again Make FTC Great Again Make FTC Great Again ', 
+    'هلا شباب معكم دحومي ثلاث تسعات هلا شباب معكم دحومي ثلاث تسعات هلا شباب معكم دحومي ثلاث تسعات هلا شباب معكم دحومي ثلاث تسعات هلا شباب معكم دحومي ثلاث تسعات هلا شباب معكم دحومي ثلاث تسعات '
+    ];
+  final candidateImages = ['1', '2', '3'];
+
+}
+
+class Candidate {
+  String name;
+  String plan;
+  int image;
+
+  Candidate(name, plan, image);
+
+  String getName() {
+    return name;
   }
 }
