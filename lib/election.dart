686fd1541b3632fed873d6c0f72650cc13ba3726
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-02-01 19:49:37 +0300

    creating the tabs and setting up the views

--- lib/election.dart
+++ lib/election.dart
@@ -7,13 +7,36 @@ class ElectionPage extends StatefulWidget {
 }
 
 class ElectionPageState extends State<ElectionPage> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('انتخابات النادي')),
-      body: Center(child: Text('انتخابات النادي')),
+      body: (_selectedIndex == 0) ? _candidatesView() : _candidatureView(),
+      bottomNavigationBar: BottomNavigationBar(
+       items: <BottomNavigationBarItem>[
+         BottomNavigationBarItem(icon: Icon(IconData(0xe8d3, fontFamily: 'MaterialIcons')), title: Text('المرشحين')),
+         BottomNavigationBarItem(icon: Icon(IconData(0xe3bb, fontFamily: 'MaterialIcons')), title: Text('الترشح')),
+       ],
+       currentIndex: _selectedIndex,
+       fixedColor: Colors.deepPurple,
+       onTap: _onItemTapped,
+     ),
     );
   }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _candidatesView() {
+    return Center(child: Text('انتخابات النادي'));
+  }
+
+  Widget _candidatureView() {
+    return Center(child: Text('الترشح لرئاسة النادي'));
+  }
 }
-
-
